26f0c85eb2d469ab347b99ac400486f6bb756248
bladerangel <pedrorangel-10@hotmail.com>
2016-04-08 21:23:10 -0300

    refatoracao lagrange

--- src/equacao/Lagrange.java
+++ src/equacao/Lagrange.java
@@ -3,18 +3,19 @@ package equacao;
 public class Lagrange {
 
 	public double teorema(double[] equacao) throws Exception {
-		double b = equacao[equacao.length - 1];
+		double a0 = equacao[equacao.length - 1];
+		double b = a0;
 		double an = equacao[0];
 		double n = equacao.length - 1;
 		double k = 0;
 
-		if (equacao[0] > 0 && equacao[equacao.length - 1] != 0) {
+		if (an > 0 && a0 != 0) {
 
 			for (int i = equacao.length - 1; i >= 0; i--) {
 				if (equacao[i] < 0) {
 					k = n - i;
 				}
-				if (b >= equacao[i]) {
+				if (equacao[i] <= b) {
 					b = equacao[i];
 				}
 
@@ -47,22 +48,22 @@ public class Lagrange {
 
 	public double raizes(double[] equacao) throws Exception {
 
-			double l = teorema(equacao);
-			// System.out.println(l);
+		double l = teorema(equacao);
+		// System.out.println(l);
 
-			double l1 = teorema(inverteEquacao(equacao));
-			// System.out.println(l1);
+		double l1 = teorema(inverteEquacao(equacao));
+		// System.out.println(l1);
 
-			double l2 = teorema(indicesImparesNegativosEquacao(equacao));
-			// System.out.println(l2);
+		double l2 = teorema(indicesImparesNegativosEquacao(equacao));
+		// System.out.println(l2);
 
-			double l3 = teorema(inverteEquacao(indicesImparesNegativosEquacao(equacao)));
-			// System.out.println(l3);
+		double l3 = teorema(inverteEquacao(indicesImparesNegativosEquacao(equacao)));
+		// System.out.println(l3);
 
-			System.out.println("Raízes reais positiva:" + 1 / l1 + " <= E+ <= " + l);
-			System.out.println("Raízes reais negativa:" + -l2 + " <= E- <= " + -1 / l3);
+		System.out.println("Raízes reais positiva:" + 1 / l1 + " <= E+ <= " + l);
+		System.out.println("Raízes reais negativa:" + -l2 + " <= E- <= " + -1 / l3);
 
-			return l;
+		return l;
 	}
 
 }
